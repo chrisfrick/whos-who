1499ab48206c5312089e548c358006e6095a8987
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-12-19 11:49:21 -0500

    add welcome page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { GameOverComponent } from './game-over/game-over.component';
 import { GameStatsComponent } from './components/game-stats/game-stats.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [{ path: "", component: HomeComponent }, {path: "game-over", component: GameOverComponent}];
+const routes: Routes = [
+  { path: "", component: WelcomeComponent },
+  { path: "game-over", component: GameOverComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, GameOverComponent, GameStatsComponent],
+  declarations: [AppComponent, HomeComponent, GameOverComponent, GameStatsComponent, WelcomeComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
