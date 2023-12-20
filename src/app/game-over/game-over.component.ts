0d0921f698a6a49e15046ea3a81e7c19759d7709
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2023-12-20 10:15:01 -0600

    add final score calculation to game-over

--- src/app/game-over/game-over.component.ts
+++ src/app/game-over/game-over.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/services/userService';
+import Game from '../Models/Game';
 
 @Component({
   selector: 'app-game-over',
@@ -8,12 +10,50 @@ import { Component, OnInit } from '@angular/core';
 export class GameOverComponent implements OnInit {
 
   playerName: string = ''
+  currentGame: Game = {
+    playerName: "",
+    finalScore: 0,
+    difficulty: "",
+    genres: [],
+  };
 
-
-
-  constructor() { }
+  constructor(private gameData: UserService) {}
 
   ngOnInit(): void {
+    // set current game
+    this.gameData.currentGame.subscribe(currentGame => this.currentGame = currentGame)
+
+    // check for not-undefined correctAnswers and numberOfQuestions
+    if (this.currentGame.correctAnswers && this.currentGame.numberOfQuestions) {
+      // calculate final score based on difficulty
+      switch (this.currentGame.difficulty) {
+        case 'hard':
+          this.currentGame.finalScore = this.calculateFinalScore(
+            this.currentGame.correctAnswers,
+            this.currentGame.numberOfQuestions,
+            300)
+            break
+        case 'medium':
+          this.currentGame.finalScore = this.calculateFinalScore(
+            this.currentGame.correctAnswers,
+            this.currentGame.numberOfQuestions,
+            200)
+            break
+        case 'easy':
+        default:
+          this.currentGame.finalScore = this.calculateFinalScore(
+            this.currentGame.correctAnswers,
+            this.currentGame.numberOfQuestions,
+            100) 
+      }
+    }
+    
+    // update userService with final score
+    this.gameData.updateCurrentGame(this.currentGame)
+  }
+
+  calculateFinalScore (correctAnswers: number, totalQuestions: number, difficultyMultiplier: number) {
+    return (correctAnswers / totalQuestions) * difficultyMultiplier
   }
 
   setPlayerName(name: string) {
