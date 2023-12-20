3ab911a48e839f32e31fbf5d69f3a5b052cdd328
Galen Sato <galen.sato@gmail.com>
2023-12-20 08:36:31 -0500

    created difficulty selection logic

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import Game from "../Models/Game";
+import { UserService } from "src/services/userService";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.css"],
 })
 export class SettingsComponent implements OnInit {
+  game: Game = {
+    playerName: "",
+    finalScore: 0,
+    difficulty: "",
+    genres: [],
+  };
 
-  constructor() { }
+  gameDifficulty: string = "";
+
+  constructor(private userData: UserService) {}
 
   ngOnInit(): void {
+    this.userData.currentGame.subscribe(
+      (currentGame) => (this.game = currentGame)
+    );
   }
 
+  selectDifficulty(value: string) {
+    this.gameDifficulty = value;
+  }
 }
